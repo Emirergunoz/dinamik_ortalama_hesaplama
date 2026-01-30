d4cb4598b6fbe100186d38cfa148c10d549f63a7
Emirergunoz <emir.ergunoz@istun.edu.tr>
2026-01-30 19:39:33 +0300

    Initial commit - Flutter dynamic average calculation app

---
+++ lib/widgets/harf_dropdown_widget.dart
@@ -0,0 +1,39 @@
+import 'package:dinamik_ortalama_hesaplama/constants/app_constants.dart';
+import 'package:dinamik_ortalama_hesaplama/helper/data_helper.dart';
+import 'package:flutter/material.dart';
+
+class HarfDropdownWidget extends StatefulWidget {
+  final Function onHarfSecildi;
+  const HarfDropdownWidget({required this.onHarfSecildi,super.key});
+
+  @override
+  State<HarfDropdownWidget> createState() => _HarfDropdownWidgetState();
+}
+
+class _HarfDropdownWidgetState extends State<HarfDropdownWidget> {
+  double secilenHarfDegeri=4;
+  
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+      alignment: Alignment.center,
+      padding: Sabitler.dropDownPadding,
+      decoration: BoxDecoration(
+        color: Sabitler.anaRenk.shade100.withValues(alpha: 0.3),
+        borderRadius: Sabitler.borderRadius,
+      ),
+      child: DropdownButton<double>(
+        value: secilenHarfDegeri,
+        elevation: 16,
+        iconEnabledColor: Sabitler.anaRenk.shade200,
+        onChanged:(deger){
+          setState(() {
+            secilenHarfDegeri=deger!;
+            widget.onHarfSecildi(secilenHarfDegeri);
+          });
+        },
+        items: DataHelper.createDersHarfNotlari(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/kredi_dropdown_widget.dart
@@ -0,0 +1,38 @@
+import 'package:dinamik_ortalama_hesaplama/constants/app_constants.dart';
+import 'package:dinamik_ortalama_hesaplama/helper/data_helper.dart';
+import 'package:flutter/material.dart';
+
+class KrediDropdownWidget extends StatefulWidget {
+  final Function onKrediSecildi;
+  const KrediDropdownWidget({required this.onKrediSecildi,super.key});
+
+  @override
+  State<KrediDropdownWidget> createState() => _KrediDropdownWidgetState();
+}
+
+class _KrediDropdownWidgetState extends State<KrediDropdownWidget> {
+  double secilenKrediDegeri=1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: Sabitler.dropDownPadding,
+      decoration: BoxDecoration(
+        color: Sabitler.anaRenk.shade100.withValues(alpha: 0.3),
+        borderRadius: Sabitler.borderRadius,
+      ),
+      child: DropdownButton<double>(
+        value: secilenKrediDegeri,
+        elevation: 16,
+        iconEnabledColor: Sabitler.anaRenk.shade200,
+        onChanged:(deger){
+          setState(() {
+            secilenKrediDegeri=deger!;
+            widget.onKrediSecildi(deger);
+          });
+        },
+        items: DataHelper.tumKrediler(),
+      ),
+    ); 
+  }
+}
